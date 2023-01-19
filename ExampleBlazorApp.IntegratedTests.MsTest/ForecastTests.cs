2b5318c507f5eda93f12ce6c3ca2ec2b36222d61
Mike Curn <mjcurn@gmail.com>
2023-01-18 18:56:34 -0800

    Update ForecastTests.cs

--- ExampleBlazorApp.IntegratedTests.MsTest/ForecastTests.cs
+++ ExampleBlazorApp.IntegratedTests.MsTest/ForecastTests.cs
@@ -18,3 +18,15 @@ public class ForecastTests : PageTest
         await Expect(Page.Locator(ForecastTable)).ToBeEnabledAsync();
     }
 }
+
+[TestClass]
+public class ForecastApiTests : PlaywrightTest
+{
+    [TestMethod]
+    public async void FetchData()
+    {
+        IAPIRequestContext request = await Playwright.APIRequest.NewContextAsync();
+        IAPIResponse response = await request.GetAsync("https://localhost:7257/WeatherForecast");
+        await Expect(response).ToBeOKAsync();
+    }
+}
